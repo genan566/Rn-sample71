44c30058da1308787800b4d3d08804cf3c5b3769
genan566 <avodagbejean@gmail.com>
2023-03-11 23:34:58 +0100

    adding ui-kitten tabs for TestApp

--- App.tsx
+++ App.tsx
@@ -5,22 +5,41 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-
+import * as eva from '@eva-design/eva';
 import PushNotification from "react-native-push-notification";
+import { ApplicationProvider, Icon, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
+
 // const Tab = createMaterialTopTabNavigator();
 
-const Feed = () => {
+const NotificationScreen = () => {
   return (
     <View>
-      <Text>Feed</Text>
+      <Text>NotificationScreen</Text>
     </View>
   )
 }
 
-const Article = () => {
+const TextScreen = () => {
+  return (
+    <View>
+      <Text>TextScreen</Text>
+    </View>
+  )
+}
+
+const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text>CalculatorScreen</Text>
+    </View>
+  )
+}
+
+const PhotoScreen = () => {
 
   const handleNotification = () => {
     PushNotification.localNotification({
@@ -36,7 +55,7 @@ const Article = () => {
           handleNotification()
         }}
       >
-        <Text>Article</Text>
+        <Text>PhotoScreen</Text>
       </Pressable>
     </View>
   )
@@ -60,13 +79,140 @@ function App() {
 
   return (
 
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Drawer.Screen name="Feed" component={Feed} />
-        <Drawer.Screen name="dgg" component={Feed} />
-        <Drawer.Screen name="Article" component={Article} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <>
+
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <NavigationContainer>
+          {/* <Tab.Navigator>
+        <Drawer.Screen name="Notification" component={Notification} />
+        <Drawer.Screen name="dgg" component={Notification} />
+        <Drawer.Screen name="PhotoScreen" component={PhotoScreen} />
+      </Tab.Navigator> */}
+          <Tab.Navigator
+            initialRouteName='Notification'
+
+            screenOptions={{
+              tabBarStyle: {
+                backgroundColor: "#1a202c",
+                borderColor: "transparent",
+                borderWidth: 0,
+                borderTopColor: "transparent",
+              },
+
+              // showLabel: false,
+              tabBarShowLabel: false,
+
+            }}
+          >
+
+            <Tab.Screen
+              name={"Notification"}
+              component={NotificationScreen}
+              // options={{
+
+              // }}
+
+              options={{
+                tabBarLabelStyle: {
+                  fontSize: 11.3,
+                  fontFamily: "Montserrat-SemiBold",
+                },
+                headerShown: false,
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Icon name="bell-outline" style={{
+                    width: focused ? 25 : 20,
+                    height: focused ? 25 : 20,
+                    tintColor: focused ? "#38b2ac" : "white",
+                  }} />
+                  // <IconMaterial name="event" size={} color={color} />
+                )
+              }}
+            />
+
+            <Tab.Screen
+              name={"Photo"}
+              component={PhotoScreen}
+              options={{
+                tabBarLabelStyle: {
+                  fontSize: 11.3,
+                  fontFamily: "Montserrat-SemiBold",
+                },
+                headerShown: false,
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Icon name="camera" style={{
+                    width: focused ? 25 : 20,
+                    height: focused ? 25 : 20,
+                    tintColor: focused ? "#38b2ac" : "white",
+                  }} />
+                  // <IconMaterial name="event" size={} color={color} />
+                )
+              }}
+            />
+
+            <Tab.Screen
+              name={"Text"}
+              component={TextScreen}
+              options={{
+                tabBarLabelStyle: {
+                  fontSize: 11.3,
+                  fontFamily: "Montserrat-SemiBold",
+                },
+                headerShown: false,
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Icon name="file-text-outline" style={{
+                    width: focused ? 25 : 20,
+                    height: focused ? 25 : 20,
+                    tintColor: focused ? "#38b2ac" : "white",
+                  }} />
+                  // <IconMaterial name="event" size={} color={color} />
+                )
+              }}
+            />
+
+            <Tab.Screen
+              name={"Calculator"}
+              component={CalculatorScreen}
+              options={{
+                tabBarLabelStyle: {
+                  fontSize: 11.3,
+                  fontFamily: "Montserrat-SemiBold",
+                },
+                headerShown: false,
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Icon name="camera" style={{
+                    width: focused ? 25 : 20,
+                    height: focused ? 25 : 20,
+                    tintColor: focused ? "#38b2ac" : "white",
+                  }} />
+                  // <IconMaterial name="event" size={} color={color} />
+                )
+              }}
+            />
+
+            {/* <Tab.Screen
+          name={"Mes Ordres"}
+          component={MyOrders}
+          options={{
+            tabBarLabelStyle: {
+              fontSize: 11.3,
+              fontFamily: "Montserrat-SemiBold",
+            },
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => (
+              <Icon name="list" style={{
+                width: focused ? 25 : 20,
+                height: focused ? 25 : 20,
+                tintColor: focused ? "#38b2ac" : "white",
+              }} />
+              // <IconMaterial name="event" size={} color={color} />
+            )
+          }}
+        /> */}
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
+    </>
     // <Text>sdfjh</Text>
   );
 }
