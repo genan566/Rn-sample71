84ec8ab103577decec286fb94e9d1889d650d0aa
genan566 <avodagbejean@gmail.com>
2023-03-12 01:28:31 +0100

    first implememtation of button photos

--- screens/PhotoScreen.js
+++ screens/PhotoScreen.js
@@ -1,8 +1,13 @@
-import { Pressable, Text, View } from "react-native"
+import { Image, Pressable, Text, View } from "react-native"
 import React from 'react'
 import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 
 const PhotoScreen = () => {
+    const [photo, setPhoto] = React.useState('')
+
+    React.useEffect(() => {
+        console.log("photo", photo)
+    }, [photo])
 
     const loadCamera = () => {
         let options = {
@@ -13,6 +18,16 @@ const PhotoScreen = () => {
             includeBase64: true,
         }
         launchCamera(options, (res) => {
+            if (res.didCancel) {
+                console.log("Cancelled")
+            } else if (res.errorCode) {
+                console.log("Error Code: " + res.errorCode)
+            }
+
+            else {
+                const source = { uri: res.assets[0].uri }
+                setPhoto(source)
+            }
         });
     }
 
@@ -25,11 +40,29 @@ const PhotoScreen = () => {
             includeBase64: true,
         }
         launchImageLibrary(options, (res) => {
+            if (res.didCancel) {
+                console.log("Cancelled")
+            } else if (res.errorCode) {
+                console.log("Error Code: " + res.errorCode)
+            }
+
+            else {
+                const source = { uri: res.assets[0].uri }
+                setPhoto(source)
+                // console.log("assertPhoto", res.assets[0].uri)
+            }
         });
     }
 
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center", paddingHorizontal: 15, gap: 10 }}>
+            {
+                photo && <View>
+                    <Image
+                        source={photo}
+                        style={{ width: 100, height: 100 }} />
+                </View>
+            }
             <Pressable onPress={() => loadCamera()}
                 style={{ width: "100%", paddingVertical: 10, borderRadius: 5, backgroundColor: "red", alignItems: "center", justifyContent: "center", }}>
                 <Text style={{ color: "white" }}>Upload By Camera</Text>
